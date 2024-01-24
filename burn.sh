3199c377564345f2e97cf1375f646c28f194f24d
Varelas Panagiotis <varelaspanos@gmail.com>
2024-01-24 16:27:21 +0200

    Fix Makefile (avrdude command)

--- burn.sh
+++ burn.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-sudo avrdude -p m328 -c usbasp -U flash:w:lightsout.hex:i
+sudo avrdude -p m328p -c usbasp -U lfuse:w:0xe2:m -U hfuse:w:0xdf:m -U efuse:w:0xff:m -U flash:w:lightsout.hex:i
