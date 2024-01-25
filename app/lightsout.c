803656a91cbbc6d44c5cb85b4f7393c493b2f406
Varelas Panagiotis <varelaspanos@gmail.com>
2024-01-25 17:48:18 +0200

    Introduce app_init function

--- app/lightsout.c
+++ app/lightsout.c
@@ -1,16 +1,24 @@
-#include <util/delay.h>
 #include <stdlib.h>
+#include <util/delay.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
+
+#define COLS 8
+#define ROWS 5
+
+// Frame buffers for the LED matrix
+uint8_t currentState[COLS][ROWS];
+uint8_t targetState[COLS][ROWS];
 
 void mcu_init (void) {
         // speaker and led matrix rows (through transistor array)
         DDRC = 0b00111111;
-        // button matrix rows, mode leds and main button
+        // mode indicator, main button and button matrix rows
         DDRB = 0b11011111;
-        // this will be changing, depending on wether
-        // we read the buttons or drive the leds
-        DDRD = 0b11111111;
+        // this will be constantly changing
+        // - input for reading the button matrix
+        // - output for driving the led matrix
+        DDRD = 0b00000000;
 
         // disable analog to digital converter
         ADCSRA = ADCSRA & ~(1<<ADEN);
@@ -27,6 +35,18 @@ void mcu_init (void) {
         //out MCUCR, tmp
 }
 
+void app_init (void) {
+        for (int x = 0; x < COLS; x++) {
+                for (int y = 0; y < ROWS; y++) {
+                        currentState[x][y] = 0;
+                        targetState[x][y] = 0;
+                }
+        }
+}
+
 int main(void) {
         mcu_init();
+        app_init();
+
+
 }
